305284178f107f7c75b59b171dd738ed0e45bfc7
salvarezri <santiagoalvarezricardo@gmail.com>
2022-02-07 14:42:27 -0500

    add something

--- js/app/app.js
+++ js/app/app.js
@@ -4,6 +4,7 @@ let filters = loadFilters();
 let markersApp = initMarkers(filters);
 let property;
 let markerClust;
+let http_request = false;
 
 console.log(JSON.stringify(markersApp));
 console.log(JSON.stringify(markersApp,['pos','lat', 'lng', 'idHouse']));
@@ -79,4 +80,62 @@ function initMap() {
 }
 function updateMarkers(newMarkers){
     markersApp = newMarkers;
+}
+
+function makeRequest(url, data = null, req) {
+
+    http_request = false;
+
+    if (window.XMLHttpRequest) { // Mozilla, Safari,...
+        http_request = new XMLHttpRequest();
+        if (http_request.overrideMimeType) {
+            http_request.overrideMimeType('text/xml');
+            // Ver nota sobre esta linea al final
+        }
+    } else if (window.ActiveXObject) { // IE
+        try {
+            http_request = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                http_request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {}
+        }
+    }
+
+    if (!http_request) {
+        alert('Falla :( No es posible crear una instancia XMLHTTP');
+        return false;
+    }
+    if(req ==0){
+        // load all markers
+        
+        http_request.onreadystatechange = alertContents;
+        http_request.open('GET', url, false);
+        http_request.send();
+    }
+    else if(req == 1){
+        // make filter
+        http_request.onreadystatechange = alertContents;
+        http_request.open('POST', url, false);
+        http_request.send(data);
+    }
+    else if( req == 2){
+        // load info House
+        http_request.onreadystatechange = alertContents;
+        http_request.open('POST', url, false);
+        http_request.send(data);
+    }
+
+}
+
+function alertContents() {
+
+    if (http_request.readyState == 4) {
+        if (http_request.status == 200) {
+            alert(http_request.responseText);
+        } else {
+            alert('Hubo problemas con la petición.');
+        }
+    }
+
 }
